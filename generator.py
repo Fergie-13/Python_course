01af7b6fce505e54497485db50d6fc018b14090d
unknown <yanchilenko98@gmail.com>
2022-06-22 21:35:25 +0300

    homework Generators

---
+++ generator.py
@@ -0,0 +1,11 @@
+def gen_number():
+    num = 1
+    while True:
+        if num % 3 == 0:
+            print('Василий')
+
+        yield num
+        num += 1
+gen = gen_number()
+for _ in range(1,11):
+    print(next(gen))
